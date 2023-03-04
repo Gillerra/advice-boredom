4f3ffdf46fa6a18ae49b2860a556cd417e0669e6
Gillerra <120829788+Gillerra@users.noreply.github.com>
2023-03-04 20:34:05 +0300

    first commit

--- src/Advices.js
+++ src/Advices.js
@@ -1,28 +1,11 @@
-import { gsap } from "gsap";
-import { useEffect, useRef } from 'react';
 function Advices () {
 
-    const textAnime = useRef();
-
-    useEffect(() => {
-        gsap.to(".heading", {y: -20,
-        duration: 3
-       
-    });
-    gsap.to(".text", {rotation: 360,
-        duration: 2,
-        delay: 0.7
-    });
-        
-    }, []);
-
-
     return(<div>
-    <div className="heading" ref={textAnime}> 
+    <div className="heading"> 
         <h1>Take advantage of our tips</h1>
     </div>
     
-    <div className="text" ref={textAnime}>
+    <div className="text">
         <h3>Are you bored? Don't know what to do?</h3>
         <h2>Look at the advice:</h2>
         
@@ -32,3 +15,4 @@ function Advices () {
 }
     
 export default Advices;
+
